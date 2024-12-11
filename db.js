02076dd71a4605ee00de9a0a87c44d58a9f8490f
sHRLo <62970136+sHRLo@users.noreply.github.com>
2024-12-11 09:00:17 +0000

    My commit message

--- db.js
+++ db.js
@@ -6,10 +6,10 @@ let connection;
 export const connectToDatabase = async () => {
     if(!connection) {
         connection = await mysql.createConnection({
-            host: process.env["DB_HOST "],
-            user: process.env["DB_USER"],
-            password: process.env["DB_PASSWORD"],
-            database: process.env["DB_NAME"]
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'PM'
         })
     }
     return connection;
--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import Home from "./components/Home/Home.jsx";
 // import backgroundImage from './assets/0011.jpg';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} /><Route/>
-                <Route path="/submit" element={<Submit/>} /><Route/>
-                <Route path="/home" element={<Home/>} /><Route/>
-            </Routes>
-        </BrowserRouter>
-        // <div className='text-white h-[100vh] flex items-center justify-center bg-cover' style={{'backgroundImage': "url('./src/assets/0011.jpg')" }}>
-        //     <Login />
-        // </div>
-    )
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />} /><Route/>
+            <Route path="/submit" element={<Submit/>} /><Route/>
+            <Route path="/home" element={<Home/>} /><Route/>
+        </Routes>
+    </BrowserRouter>
+  )
 }
 export default App;
